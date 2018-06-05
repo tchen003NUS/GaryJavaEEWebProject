6589c8c510dbd3e5ed79761b834c9befe1547f0f
LAPTOP-FQ0RAQNO\Chen Tianqi <e0267854@u.nus.edu>
2018-06-05 15:09:21 +0800

    20180605 1507 1st commit by Gary

---
+++ src/com/javahelps/helloworld/HelloWorldServlet.java
@@ -0,0 +1,51 @@
+package com.javahelps.helloworld;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorldServlet
+ */
+@WebServlet(description = "Symphony", urlPatterns = { "/HelloWorldServlet" })
+public class HelloWorldServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public HelloWorldServlet(){
+    	
+    	super();
+    	
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		
+		PrintWriter out = response.getWriter();
+        out.print("<html><body><h1 align='center'>" +
+        new Date().toString() + "</h1></body></html>");
+		//System.out.print("Hello World");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
